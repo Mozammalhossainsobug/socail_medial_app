b400a19c740acebf3384c66a296a99d639701ffe
Mozammalhossainsobug <mozammalruet@gmail.com>
2023-06-05 15:57:06 +0600

    passing value as "postEntity"

--- lib/src/features/post/presentation/create_post/pages/create_post_page.dart
+++ lib/src/features/post/presentation/create_post/pages/create_post_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:socail_medial_app/src/features/post/presentation/create_post/bloc/create_post_bloc.dart';
+import 'package:socail_medial_app/src/features/post/root/domain/entities/post_entity.dart';
 
 class CreatePostPage extends StatelessWidget {
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _bodyController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+  final int num = 1;
+
+  CreatePostPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,44 +25,81 @@ class CreatePostPage extends StatelessWidget {
                 .showSnackBar(SnackBar(content: Text(state.message)));
           }
           if (state is CreatePostSuccessState) {
-            ScaffoldMessenger.of(context)
-                .showSnackBar(SnackBar(content: Text('Post successfully created')));
+            ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Post successfully created')));
             _titleController.clear();
             _bodyController.clear();
             Navigator.pop(context);
           }
         },
         builder: (context, state) {
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                TextField(
-                  controller: _titleController,
-                  decoration: const InputDecoration(
-                    labelText: 'Title',
+          return Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(
+                      labelText: 'Title',
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(10.0))),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter Tittle';
+                      }
+                      return null;
+                    },
                   ),
-                ),
-                TextField(
-                  controller: _bodyController,
-                  decoration: const InputDecoration(
-                    labelText: 'Description',
+                  const SizedBox(
+                      height:
+                          20.0), // This gives more space between the title and description fields.
+                  TextFormField(
+                    controller: _bodyController,
+                    maxLines: 5,
+                    textAlign: TextAlign
+                        .start, // This makes the field expand as more lines are added.
+                    decoration: const InputDecoration(
+                      labelText: 'Description',
+                      fillColor: Colors.white,
+                      alignLabelWithHint: true,
+                      filled: true,
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(10.0))),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter Description';
+                      }
+                      return null;
+                    },
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    context.read<CreatePostBloc>().add(AddPostEvent(
-                          userId: 1,
-                          id : 1,
-                          title: _titleController.text,
-                          body: _bodyController.text,
-                        ));
-                  },
-                  child: state is CreatePostLoadingState
-                      ? const CircularProgressIndicator()
-                      : const Text('Create Post'),
-                ),
-              ],
+                  const SizedBox(height: 20.0),
+                  Center(
+                    // This will center the button.
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if(_formKey.currentState!.validate()){
+                          final newPost = PostEntity(userId: 1, id: 1, title: _titleController.text, body: _bodyController.text);
+                          BlocProvider.of<CreatePostBloc>(context).add(AddPostEvent(
+                            newPost: newPost,
+                          ));
+                        }
+                      },
+                      child: state is CreatePostLoadingState
+                          ? const CircularProgressIndicator()
+                          : const Text('Create Post'),
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         },
